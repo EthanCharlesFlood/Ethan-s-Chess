b92b1b3822402c2dd055467348d552e4faff41c4
Ethan Flood <ethan.flood@gmail.com>
2018-09-19 16:07:06 -0400

    add simple board evaluation algorithm

--- Chess/board_evaulator.rb
+++ Chess/board_evaulator.rb
@@ -8,14 +8,24 @@ class BoardEvaluator
     King => 900
   }
 
-  def initialize(board, player1, player2)
+  def initialize(board, player, player2)
     @board = board
     @player1 = player1
     @player2 = player2
   end
 
-  def current_score
+  def simple_score
+      score = 0
+      
+      @player1.pieces.each do |piece|
+        score += VALUES[piece.class]
+      end
 
+      @player2.pieces.each do |piece|
+        score -= VALUES[piece.class]
+      end
+
+      score
   end
 
 end
