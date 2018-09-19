e90aad006809387719e14f73951193a4a296b67b
Ethan Flood <ethan.flood@gmail.com>
2018-09-19 15:54:23 -0400

    add board evaluator class

--- Chess/computer_player.rb
+++ Chess/computer_player.rb
@@ -47,12 +47,5 @@ class ComputerPlayer < Player
   end
 
   # implementation which evaluates piece value and acts accordingly
-  VALUES = { Pawn => 10,
-    Knight => 30,
-    Bishop => 30,
-    Rook => 50,
-    Queen => 90,
-    King => 900
-  }
 
 end
